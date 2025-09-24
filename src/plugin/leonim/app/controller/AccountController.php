974ad27754fd84643140d51ba65b6999fcca3699
Leon <897645119@qq.com>
2025-09-24 22:38:05 +0800

    add: AccountController.php Base.php

---
+++ src/plugin/leonim/app/controller/AccountController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace plugin\leonim\app\controller;
+
+use support\Request;
+use support\Response;
+
+/**
+ * 管理员账户
+ */
+class AccountController extends Base
+{
+
+    /**
+     * 登录
+     * @param Request $request
+     * @return Response
+     * @throws BusinessException
+     */
+    public function login(Request $request): Response
+    {
+
+    }
+
+    /**
+     * 退出
+     * @param Request $request
+     * @return Response
+     */
+    public function logout(Request $request): Response
+    {
+
+    }
+
+    /**
+     * 获取登录信息
+     * @param Request $request
+     * @return Response
+     */
+    public function info(Request $request): Response
+    {
+
+    }
+
+    /**
+     * 更新
+     * @param Request $request
+     * @return Response
+     */
+    public function update(Request $request): Response
+    {
+
+    }
+
+    /**
+     * 修改密码
+     * @param Request $request
+     * @return Response
+     */
+    public function password(Request $request): Response
+    {
+
+    }
+
+}
